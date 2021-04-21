17f4923647507aff4d6b050e73df103fa7283878
RossMurr4y <RossMurr4y@users.noreply.github.com>
2021-04-21 10:24:23 +0000

    docs: latest crate documentation

---
+++ docs/doc/rpr/cli/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["process_args","Acts as both the definition for the structure of the CLI, as well as the entrance to it. The CLI is based on the `clap` crate, and has all of the out of the box features that you would expect."]]});
